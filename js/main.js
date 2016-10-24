e4d3141ff7e155b083cada65216ec360cd0aa4c3
VanniaJimenez <patsymena11@gmail.com>
2016-10-24 09:46:26 -0500

    Solo faltan los zapatos

--- js/main.js
+++ js/main.js
@@ -1,42 +1,44 @@
-  
 var tarea = document.getElementById("tarea");
 var lista = document.getElementById ("lista");
-
 var boton = document.getElementById("boton");
 
-var close = document.getElementsByClassName("close");
-
-var quitar = document.getElementsByTagName("li");
-
-
-
-
-
-
-
-var adios = document.getElementById("lista").value = "";
- for (i = 0; i < close.length; i++) { 
- var span = document.createElement("SPAN");
-  var txt = document.createTextNode("\u00D7");
-  span.className = "close";
-  span.appendChild(txt);
-  adios[i].appendChild(span);
- };
 
 boton.onclick = function(){
 	funcional();
 };
+
 
 function crear(){
 	var box=document.createElement("input");
 	box.type = "checkbox";
   	var li = document.createElement("li");
   	var t = document.createTextNode(tarea.value);
-   	li.appendChild(box);
+  
+   	//
+    var borrar = document.createElement("SPAN");
+	var borrar_txt = document.createTextNode("\u00D7");
+	borrar.className = "close";
+	borrar.appendChild(borrar_txt);
+    
+	
+	li.appendChild(box);
    	li.appendChild(t);
-   	lista.appendChild(li);
-   	tarea.value = "";
-}
+   	li.appendChild(borrar);
+	lista.appendChild(li);
+	
+	
+	  for (i = 0; i < close.length; i++) {
+    close[i].onclick = function() {
+      var div = this.parentElement;
+      div.style.display = "none";
+    }
+	  }
+	
+
+
+	tarea.value = "";
+	
+};
 
 function funcional() {
   console.log(tarea.value);
@@ -47,18 +49,21 @@ function funcional() {
   }
 };
 
- document.getElementById("tarea").value = "";
-  var span = document.createElement("SPAN");
-  var txt = document.createTextNode("\u00D7");
-  span.className = "close";
-  span.appendChild(txt);
-  li.appendChild(span);
-  for (i = 0; i < close.length; i++) {
-    close[i].onclick = function() {
-      var div = this.parentElement;
-      div.style.display = "none";
-    }
+
+var close = document.getElementsByClassName("close");
+var i;
+for (i = 0; i < close.length; i++) {
+  close[i].onclick = function() {
+    var div = this.parentElement;
+    div.style.display = "none";
   }
+}
 
 
 
+var checked = document.getElementById("input");
+
+checked.addEventListener('click', function(ev) {
+
+	alert("CHECKED");
+}, false);
